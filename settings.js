3dd9d1268b40dd371a7ab690590c4c8ab6c2bab7
Hkima <162895330+Kirivaitete@users.noreply.github.com>
2025-01-15 08:32:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,18 +5,18 @@ You don't have to set this if you deploy using heroku because you can simply set
 
 const session = process.env.SESSION || '';
 
-const prefix = process.env.PREFIX || '';
-const mycode = process.env.CODE || "254";
-const author = process.env.STICKER_AUTHOR || 'Keith';
-const packname = process.env.PACKNAME || 'keith';
-const dev = process.env.DEV || '254748387615';
-const DevKeith = dev.split(",");
-const botname = process.env.BOTNAME || 'KEITH-MD';
+const prefix = process.env.PREFIX || '$';
+const mycode = process.env.CODE || "91";
+const author = process.env.STICKER_AUTHOR || 'Bawihpuia';
+const packname = process.env.PACKNAME || 'Bawihpuia';
+const dev = process.env.DEV || '919233092990';
+const DevKeith = dev.split("$");
+const botname = process.env.BOTNAME || 'Bawihpuia';
 const mode = process.env.MODE || 'public';
 const gcpresence = process.env.GC_PRESENCE || 'false';
 const antionce = process.env.ANTIVIEWONCE || 'true';
 const sessionName = "session";
-const presence = process.env.WA_PRESENCE || 'online';
+const presence = process.env.WA_PRESENCE || 'offline';
 
 const antitag = process.env.ANTITAG || 'true';
 const api = process.env.API || 'true';
@@ -24,15 +24,15 @@ const appname = process.env.APPNAME || 'true';
 const antispam = process.env.ANTISPAM || 'true';
 const chatbot = process.env.CHAT_BOT || 'true';
 const antilink = process.env.ANTILINK || 'true';
-const autoreact = process.env.AUTOREACT || 'true';
+const autoreact = process.env.AUTOREACT || 'false';
 const antibot = process.env.ANTIBOT || 'true';
 const anticall = process.env.ANTICALL || 'true';
-const antibad = process.env.ANTI_BAD_WORD || 'true';
+const antibad = process.env.ANTI_BAD_WORD || 'false';
 const antidelete = process.env.ANTIDELETE || 'true';
 const autoview = process.env.AUTOVIEW_STATUS || 'true';
 const autolike = process.env.AUTOLIKE_STATUS || 'true';
 const permit = process.env.PM_PERMIT || 'true';
-const autoread = process.env.AUTOREAD || 'true';
+const autoread = process.env.AUTOREAD || 'false';
 const autobio = process.env.AUTOBIO || 'false';
 
 module.exports = {
